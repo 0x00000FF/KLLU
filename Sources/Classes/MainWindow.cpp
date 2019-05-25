115a665801bb34aa0ec39941bfe24fe196a6a839
P.Knowledge <knowledge@patche.me>
2019-05-25 17:41:12 +0900

    Basic MainWindow Behavior

--- Sources/Classes/MainWindow.cpp
+++ Sources/Classes/MainWindow.cpp
@@ -1,11 +1,25 @@
 #include <KLLUInterface/MainWindow.hpp>
 
-MainWindow::MainWindow() 
-{
-
-}
 
 const int MainWindow::execResult() const
 {
     return m_execResult;
+}
+
+
+void MainWindow::_beginWindowLoop()
+{
+    sf::Event ev;
+
+    while (this->isOpen())
+    {
+        this->pollEvent(ev);
+
+        switch (ev.type)
+        {
+
+        }
+
+        this->display();
+    }
 }
--- Sources/Includes/KLLUInterface/MainWindow.hpp
+++ Sources/Includes/KLLUInterface/MainWindow.hpp
@@ -7,9 +7,16 @@ class MainWindow : public sf::RenderWindow
     int m_execResult = 0;
 
 public:
-    MainWindow();
+    MainWindow() : RenderWindow({ 300, 300 }, L"Knowledge's Logic Unit") 
+    {
+        _beginWindowLoop();
+    }
+
     MainWindow(const MainWindow& _window) = delete;
     MainWindow(MainWindow&&) = default;
 
     const int execResult() const;
+
+private:
+    void _beginWindowLoop();
 };
