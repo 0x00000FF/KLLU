51981e8b43d2421ffa2869f34a7a54f3b8e396f8
P.Knowledge <knowledge@patche.me>
2019-05-25 12:28:39 +0900

    CMake project setup

---
+++ Sources/KLLU.cpp
@@ -0,0 +1,20 @@
+﻿// KLLU.cpp : Defines the entry point for the application.
+//
+
+#include <KLLU.h>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main()
+{
+    auto rw = sf::RenderWindow({300, 300}, "system");
+    while (rw.isOpen())
+    {
+        sf::Event ev;
+
+        switch (rw.pollEvent(ev)) {}
+        rw.display();
+    }
+	return 0;
+}
